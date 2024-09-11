c9131b9d962dd98cc01641b867a9fe2b3511439e
Igor Nunes <101185370+igornunes7@users.noreply.github.com>
2024-09-11 17:29:49 -0400

    Update main.c

--- Trab 5/main.c
+++ Trab 5/main.c
@@ -6,25 +6,24 @@
 struct s_no {
     int num;
     struct s_no *prox;
-    pthread_mutex_t lock; 
+    pthread_mutex_t lock;
 };
+
 typedef struct s_no s_no;
 
 s_no *ptlista1 = NULL;
+pthread_mutex_t lock;
 
 void *removerPares(void *param);
 void *removerPrimos(void *param);
 void *imprimirPrimos(void *param);
 
-
-//função para inicializar o nó cabeça da lista
 void inicializaPt(s_no **ptlista) {
-    //assegura que ptlista é valido e *ptlista nao foi iniciado ainda
     if ((ptlista != NULL) && (*ptlista == NULL)) {
         *ptlista = malloc(sizeof(s_no));
         if (*ptlista != NULL) {
             (*ptlista)->prox = NULL;
-            (*ptlista)->num = -1;
+            (*ptlista)->num = -99;
             pthread_mutex_init(&(*ptlista)->lock, NULL);
         } else {
             fprintf(stderr, "Erro na alocacao do no cabeca.\n");
@@ -36,7 +35,6 @@ void inicializaPt(s_no **ptlista) {
     }
 }
 
-//função para inserir no final da lista1
 void inserirFim(s_no **ptlista, int x) {
     s_no *novo = malloc(sizeof(s_no));
     novo->num = x;
@@ -44,71 +42,92 @@ void inserirFim(s_no **ptlista, int x) {
     pthread_mutex_init(&novo->lock, NULL);
 
     s_no *ptr = *ptlista;
+
     while (ptr->prox != NULL) {
         ptr = ptr->prox;
     }
 
-    pthread_mutex_lock(&novo->lock);
+    pthread_mutex_lock(&ptr->lock);
     ptr->prox = novo;
-    pthread_mutex_unlock(&novo->lock);
+    pthread_mutex_unlock(&ptr->lock);
 }
 
-//função auxiliar para verificar se o número é primo
 bool isPrimo(int n) {
-    if (n <= 1) return false;
-    if (n == 2) return true;
-    if (n % 2 == 0) return false;
+    if (n <= 1) {
+        return false;
+    }
+    if (n == 2) {
+        return true;
+    }
+    if (n % 2 == 0) {
+        return false;
+    }
     for (int i = 3; i * i <= n; i += 2) {
-        if (n % i == 0) return false;
+        if (n % i == 0) {
+            return false;
+        }
     }
     return true;
 }
 
-//desalocar a lista
-void desalocarLista(s_no **ptlista) {
-    s_no *ptr = *ptlista;
+void desalocarLista(s_no *ptlista) {
+    s_no *ptr1 = ptlista;
     s_no *aux;
 
-    while (ptr != NULL) {
-        aux = ptr->prox;
-        pthread_mutex_destroy(&ptr->lock);
-        free(ptr);
-        ptr = aux;
+    while (ptr1 != NULL) {
+        aux = ptr1->prox;
+        pthread_mutex_destroy(&ptr1->lock);
+        free(ptr1);
+        ptr1 = aux;
     }
-
-    *ptlista = NULL; 
+    ptlista = NULL;
 }
 
-//função para remover os pares maiores que 2 da lista1
 void *removerPares(void *param) {
-    s_no *ptr1 = ptlista1;
+    s_no *ptr1 = ptlista1->prox;
     s_no *aux = NULL;
 
-    while (ptr1 != NULL) {
-        if (ptr1->prox->num % 2 == 0 && ptr1->prox->num > 2) {
-            pthread_mutex_lock(&ptr1->lock);
-            s_no *temp = ptr1->prox;
+    while (ptr1->num != -1) {
+        pthread_mutex_lock(&ptr1->lock);
+        if (ptr1->num % 2 == 0 && ptr1->num > 2) {
+            if (aux == NULL) {
+                ptlista1->prox = ptr1->prox;
+            } else {
+                aux->prox = ptr1->prox;
+            }
+            s_no *temp = ptr1;
             pthread_mutex_unlock(&ptr1->lock);
+            ptr1 = ptr1->prox;
             pthread_mutex_destroy(&temp->lock);
             free(temp);
+
+            
         } else {
+            pthread_mutex_unlock(&ptr1->lock);
             aux = ptr1;
             ptr1 = ptr1->prox;
         }
     }
+
     pthread_exit(0);
 }
 
-//função para remover não primos da lista1
 void *removerPrimos(void *param) {
-    s_no *ptr1 = ptlista1;
+    s_no *ptr1 = ptlista1->prox;
     s_no *aux = NULL;
 
     while (ptr1 != NULL) {
         if (!isPrimo(ptr1->num) && ptr1->num != -1) {
             pthread_mutex_lock(&ptr1->lock);
+            if (aux == NULL) {
+                ptlista1->prox = ptr1->prox;
+            } else {
+                aux->prox = ptr1->prox;
+            }
             s_no *temp = ptr1;
-            pthread_mutex_unlock(&ptr1->lock);
+            ptr1 = ptr1->prox;
+
+            pthread_mutex_unlock(&temp->lock);
             pthread_mutex_destroy(&temp->lock);
             free(temp);
         } else {
@@ -116,37 +135,41 @@ void *removerPrimos(void *param) {
             ptr1 = ptr1->prox;
         }
     }
+
     pthread_exit(0);
 }
 
-//função para imprimir a lista1
 void *imprimirPrimos(void *param) {
-    s_no *ptr1;
-    
-    ptr1 = ptlista1->prox;
+    s_no *ptr1 = ptlista1->prox;
 
     printf("Lista1: ");
-    while (ptr1 != NULL) {
-        printf("%d ", ptr1->num);
+    while (ptr1->num != -1) {
         pthread_mutex_lock(&ptr1->lock);
-        ptr1 = ptr1->prox;
+        printf("%d ", ptr1->num);
         pthread_mutex_unlock(&ptr1->lock);
+        ptr1 = ptr1->prox;
     }
+
     printf("\n");
     pthread_exit(0);
 }
 
 int main(int argc, char **argv) {
     int valor = 0;
-    
+
+    if (pthread_mutex_init(&lock, NULL) != 0) {
+        printf("\n mutex init has failed\n");
+        return 1;
+    }
+
     FILE *arq;
-    inicializaPt(&ptlista1);
+
     pthread_attr_t attr1, attr2, attr3;
-    pthread_t tid[3];
+    pthread_t tid[2];
+    s_no *ptr = NULL;
 
     pthread_attr_init(&attr1);
     pthread_attr_init(&attr2);
-    pthread_attr_init(&attr3);
 
     if (argc != 2) {
         fprintf(stderr, "usage: %s <file name>\n", argv[0]);
@@ -159,21 +182,24 @@ int main(int argc, char **argv) {
         exit(-1);
     }
 
+    inicializaPt(&ptlista1);
+
+
     pthread_create(&tid[0], &attr1, removerPares, NULL);
-    pthread_create(&tid[1], &attr2, removerPrimos, NULL);
-    pthread_create(&tid[2], &attr3, imprimirPrimos, NULL);
+    pthread_create(&tid[1], &attr2, imprimirPrimos, NULL);
 
     while (fscanf(arq, "%d", &valor) != EOF) {
         inserirFim(&ptlista1, valor);
     }
 
+    inserirFim(&ptlista1, -1); 
     fclose(arq);
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 2; i++) {
         pthread_join(tid[i], NULL);
     }
 
-    desalocarLista(&ptlista1);
+    // desalocarLista(ptlista1);
 
     return 0;
 }
